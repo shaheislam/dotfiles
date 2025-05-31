c403a6cf90a418894fd6af9175cc472d5f7ec0af
Shahe <shahes@mac.home>
2025-06-01 00:16:53 +0100

    Adding telescope dynamic project scoping

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -179,4 +179,24 @@ return {
       end
     end,
   },
+
+  -- Project management
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = false,
+      detection_methods = { "lsp", "pattern" },
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "Cargo.toml" },
+      show_hidden = false,
+      silent_chdir = true,
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      require("telescope").load_extension("projects")
+    end,
+    keys = {
+      { "<leader>fp", "<cmd>Telescope projects<cr>", desc = "Projects" },
+    },
+  },
 }
