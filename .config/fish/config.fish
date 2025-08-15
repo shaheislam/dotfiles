b18b1657b7547f62958ee889f84bba5622ca13a6
Mohammed Islam <shahedulislam94@gmail.com>
2025-08-15 02:21:18 +0100

    fzf for ps

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1130,6 +1130,67 @@ if status is-interactive
         end
     end
 
+    function psf --description "Interactive process search with fzf"
+        set -l processes (procs --color=disable)
+
+        if test -z "$processes"
+            echo "No processes found"
+            return 1
+        end
+
+        set -l selected (printf '%s\n' $processes | fzf \
+            --prompt="Process Search (ENTER=details, CTRL-K=kill, CTRL-R=refresh): " \
+            --height=80% \
+            --border \
+            --header-lines=1 \
+            --bind='ctrl-k:execute-silent(kill -9 {1})+reload(procs --color=disable)' \
+            --bind='ctrl-r:reload(procs --color=disable)' \
+            --preview='procs {1} --tree' \
+            --preview-window=right:50%:wrap)
+
+        if test -n "$selected"
+            set -l pid (echo $selected | awk '{print $1}')
+            if test "$pid" != "PID"  # Skip header if selected
+                echo "Details for PID $pid:"
+                procs $pid --tree
+            end
+        end
+    end
+
+    function psg --description "Search processes with grep and fzf"
+        if test -z "$argv[1]"
+            echo "Usage: psg <search_term>"
+            echo "Example: psg chrome"
+            return 1
+        end
+
+        set -l matches (procs --color=disable | grep -i "$argv[1]" 2>/dev/null)
+
+        if test -z "$matches"
+            echo "No processes matching '$argv[1]' found"
+            return 1
+        end
+
+        # Add header from procs
+        set -l header (procs --color=disable | head -n 1)
+        set -l selected (printf '%s\n%s\n' "$header" "$matches" | fzf \
+            --prompt="Processes matching '$argv[1]' (ENTER=details, CTRL-K=kill): " \
+            --height=80% \
+            --border \
+            --header-lines=1 \
+            --bind='ctrl-k:execute-silent(kill -9 {1})' \
+            --preview='procs {1} --tree' \
+            --preview-window=right:50%:wrap)
+
+        if test -n "$selected"
+            set -l pid (echo $selected | awk '{print $1}')
+            if test "$pid" != "PID"  # Skip header if selected
+                echo "Details for PID $pid:"
+                procs $pid --tree
+            end
+        end
+    end
+
     function port --description "Show what's listening on a given port (with fzf if no port specified)"
         if test -z "$argv[1]"
             # No port specified, use fzf to select from all listening ports
