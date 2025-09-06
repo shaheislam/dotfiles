a2a030de470672445b3660d8d8a521e3aa1a644e
Mohammed Islam <shahedulislam94@gmail.com>
2025-09-06 16:44:22 +0100

    update to 90% opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.color_scheme = 'Catppuccin Mocha'
 
 -- Window configuration
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 20
 
 -- Tab bar
