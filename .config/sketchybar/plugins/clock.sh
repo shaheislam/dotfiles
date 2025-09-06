7f5dcc07110d45647991763197910523a41ea6ba
Mohammed Islam <shahedulislam94@gmail.com>
2025-09-06 15:40:13 +0100

    add sketchybar config

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # The $NAME variable is passed from sketchybar and holds the name of
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
+TIME=$(date '+%H:%M')
+DATE=$(date '+%a %d %b')
+
+sketchybar --set "$NAME" label="$TIME  $DATE"
