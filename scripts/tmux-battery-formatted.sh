f14b993908473f66b8419d0b1ae15cfaafa0b68c
Mohammed Islam <shahedulislam94@gmail.com>
2025-09-06 14:13:34 +0100

    correct tmux status bar

--- scripts/tmux-battery-formatted.sh
+++ scripts/tmux-battery-formatted.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Get battery percentage and format without %
-battery=$(/Users/shaheislam/.tmux/plugins/tmux-battery/scripts/battery_percentage.sh | sed 's/%//')
+battery=$(~/.tmux/plugins/tmux-battery/scripts/battery_percentage.sh | sed 's/%//')
 echo "$battery"
