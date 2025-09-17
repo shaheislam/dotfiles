5bfae03c868d173e1ed326dc47724caac5640a74
Shahe Islam <shaheislam@Shahes-MacBook-Pro.local>
2025-09-17 21:37:43 +0100

    correct autocomplete

--- .config/fish/functions/_autopair_tab.fish
+++ .config/fish/functions/_autopair_tab.fish
@@ -1,5 +1,9 @@
 function _autopair_tab
-    commandline --paging-mode && down-or-search && return
+    # Check if we're in paging mode and handle it properly
+    if commandline --paging-mode
+        commandline --function down-line
+        return
+    end
 
     string match --quiet --regex -- '\$[^\s]*"$' (commandline --current-token) &&
         commandline --function end-of-line --function backward-delete-char
