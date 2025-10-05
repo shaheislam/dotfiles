81d2c689aedfaa6619aba0953b8c63e899b93233
Shahe Islam <shaheislam@Shahes-MacBook-Pro.local>
2025-10-05 21:41:28 +0100

    Correcting k9s plugins duplicates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,6 +62,9 @@ if status is-interactive
     # Colima Docker configuration
     set -x DOCKER_HOST "unix://$HOME/.colima/default/docker.sock"
 
+    # K9s configuration - use ~/.config/k9s directly
+    set -x K9S_CONFIG_DIR "$HOME/.config/k9s"
+
     # Load centralized PATH configuration
     source $HOME/.config/fish/paths.fish
 
