a5e6a02017002833daf0e7c716ed5b78f5ad22bf
Shahe Islam <shaheislam@Shahes-MacBook-Pro.local>
2025-10-07 19:06:31 +0100

    Add Telescope live grep args

--- .config/nvim/lua/plugins/telescope-live-grep-args.lua
+++ .config/nvim/lua/plugins/telescope-live-grep-args.lua
@@ -5,6 +5,14 @@
 -- - <C-k> to quote the prompt for exact phrase matching
 -- - <C-g> to insert glob pattern (case-sensitive)
 -- - <C-i> to insert iglob pattern (case-insensitive)
+-- - <C-t> to add --type filter
+-- - <C-h> to add --hidden flag
+-- - <C-space> to convert to fuzzy refine (filter results)
+-- Shortcuts:
+-- - <leader>fw to grep word under cursor (project-wide)
+-- - <leader>fW to grep word under cursor (current buffer)
+-- - <leader>fv (visual) to grep selection (project-wide)
+-- - <leader>fV (visual) to grep selection (current buffer)
 
 return {
   {
@@ -24,11 +32,15 @@ return {
                 ["<C-k>"] = lga_actions.quote_prompt(),
                 ["<C-g>"] = lga_actions.quote_prompt({ postfix = " --glob " }),
                 ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                ["<C-t>"] = lga_actions.quote_prompt({ postfix = " --type " }),
+                ["<C-h>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
               },
               n = {
                 ["<C-k>"] = lga_actions.quote_prompt(),
                 ["<C-g>"] = lga_actions.quote_prompt({ postfix = " --glob " }),
                 ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                ["<C-t>"] = lga_actions.quote_prompt({ postfix = " --type " }),
+                ["<C-h>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
               },
             },
             -- ... also accepts theme settings, for example:
@@ -49,6 +61,36 @@ return {
           require("telescope").extensions.live_grep_args.live_grep_args()
         end,
         desc = "Live Grep with Args",
+      },
+      {
+        "<leader>fw",
+        function()
+          require("telescope-live-grep-args.shortcuts").grep_word_under_cursor()
+        end,
+        desc = "Grep word under cursor",
+      },
+      {
+        "<leader>fW",
+        function()
+          require("telescope-live-grep-args.shortcuts").grep_word_under_cursor_current_buffer()
+        end,
+        desc = "Grep word under cursor (current buffer)",
+      },
+      {
+        "<leader>fv",
+        function()
+          require("telescope-live-grep-args.shortcuts").grep_visual_selection()
+        end,
+        mode = "v",
+        desc = "Grep visual selection",
+      },
+      {
+        "<leader>fV",
+        function()
+          require("telescope-live-grep-args.shortcuts").grep_word_visual_selection_current_buffer()
+        end,
+        mode = "v",
+        desc = "Grep visual selection (current buffer)",
       },
     },
   },
