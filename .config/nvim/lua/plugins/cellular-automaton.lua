fe5bc63a5f81a261a757850aab01e76c39ba790f
Shahe Islam <shaheislam@Shahes-MacBook-Pro.local>
2025-10-09 00:07:07 +0100

    add cellular-automaton

---
+++ .config/nvim/lua/plugins/cellular-automaton.lua
@@ -0,0 +1,15 @@
+return {
+  "Eandrju/cellular-automaton.nvim",
+  keys = {
+    {
+      "<leader>fml",
+      "<cmd>CellularAutomaton make_it_rain<CR>",
+      desc = "Make it rain",
+    },
+    {
+      "<leader>gol",
+      "<cmd>CellularAutomaton game_of_life<CR>",
+      desc = "Game of life",
+    },
+  },
+}
