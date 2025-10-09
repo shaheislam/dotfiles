2fd69456427168ebbee40799d8ee52f1742d505f
Shahe Islam <shaheislam@Shahes-MacBook-Pro.local>
2025-10-09 23:23:15 +0100

    Add autocmds
      - Makefile tabs enhancement
      - Quickfix Auto-Open
      - YAML/JSON 2- Indent

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -131,6 +131,45 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- ============================================================================
+-- DevOps & Cloud Formatting
+-- ============================================================================
+
+-- Enforce 2-space indentation for YAML/JSON (cloud config standard)
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("yaml_json_indent"),
+  pattern = { "yaml", "yml", "json", "jsonc" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+-- CRITICAL: Makefiles REQUIRE tabs (spaces will break make)
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("makefile_tabs"),
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+-- ============================================================================
+-- Error Handling
+-- ============================================================================
+
+-- Auto-open quickfix window when errors are detected
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  group = augroup("quickfix_auto"),
+  pattern = "[^l]*",
+  callback = function()
+    vim.cmd("cwindow")
+  end,
+})
+
+-- ============================================================================
 -- Performance Optimizations
 -- ============================================================================
 
