d4e1ff6a3b0ebad42e68bf9b31d5136ebeaae6a0
Shahe Islam <shaheislam@Shahes-MacBook-Pro.local>
2025-10-27 19:57:22 +0000

    add session.nvim and fish config

--- .config/nvim/lua/plugins/session.lua
+++ .config/nvim/lua/plugins/session.lua
@@ -1,17 +1,32 @@
 -- ~/.config/nvim/lua/plugins/session.lua
--- Session management configuration using persistence.nvim
+-- Session management configuration using nvim-possession
 -- Extracted from misc.lua for better organization
 
 return {
-  -- Configure persistence.nvim for session management
+  -- Disable persistence.nvim
   {
     "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = {},
+    enabled = false,
+  },
+
+  -- Configure nvim-possession for session management
+  {
+    "gennaro-tedesco/nvim-possession",
+    dependencies = {
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      autoload = false, -- don't auto-load sessions on startup
+      autosave = true,  -- auto-save loaded sessions before quitting
+      autoswitch = {
+        enable = false, -- don't auto-switch sessions on directory change
+      },
+    },
     keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Stop Session" },
+      { "<leader>sl", function() require("nvim-possession").list() end, desc = "List sessions" },
+      { "<leader>sn", function() require("nvim-possession").new() end, desc = "New session" },
+      { "<leader>su", function() require("nvim-possession").update() end, desc = "Update session" },
+      { "<leader>sd", function() require("nvim-possession").delete() end, desc = "Delete session" },
     },
   },
 }
