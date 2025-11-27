e9c998c737875d3ddaa3b323557e1ba1091c7b2b
Shahe Islam <shaheislam@users.noreply.github.com>
2025-11-27 22:44:07 +0000

    add kubecolor for alt-4 kubecui

--- .config/fish/functions/kubectl_fzf_native.fish
+++ .config/fish/functions/kubectl_fzf_native.fish
@@ -336,7 +336,7 @@ function kubectl_fzf_native --description "FZF-powered kubectl completion using 
         set -l f1_cmd "kubectl explain $resource_type | less"
         set -l f2_cmd "kubectl exec -it {} -- sh"
         set -l f3_cmd "kubectl get $resource_type {} -o yaml | bat --color=always -l yaml --paging=always"
-        set -l f4_cmd "kubectl describe $resource_type {} | less"
+        set -l f4_cmd "kubecolor --force-colors describe $resource_type {} | less -R"
         set -l f5_cmd "kubecolor logs -f --tail=100 {}"
         set -l f6_cmd "bash -c 'exec </dev/tty >/dev/tty 2>&1; read -p \"Local port: \" lport; read -p \"Remote port: \" rport; kubectl port-forward {} \$lport:\$rport'"
         set -l f7_cmd "kubectl debug {} -it --image=ubuntu --share-processes -- bash"
