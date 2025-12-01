ed23fc7ef27618c703454b1742e69cd078eb61eb
Shahe Islam <shaheislam@users.noreply.github.com>
2025-12-01 00:34:44 +0000

    add new testpython file

---
+++ test_pythonnew.py
@@ -0,0 +1,34 @@
+def greet(name):
+    """Return a greeting message."""
+    return f"Hi, {name}!"
+
+
+def add(a, b):
+    """Add two numbers together."""
+    return a + b
+
+
+class Counter:
+    """A simple counter class."""
+
+    def __init__(self, start=0):
+        self.value = start
+
+    def increment(self):
+        self.value += 1
+
+    def decrement(self):
+        self.value -= 1
+
+    def __str__(self):
+        return f"Counter({self.value})"
+
+
+if __name__ == "__main__":
+    print(greet("World"))
+    print(f"2 + 3 = {add(2, 3)}")
+
+    counter = Counter()
+    counter.increment()
+    counter.increment()
+    print(counter)
